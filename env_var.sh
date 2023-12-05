0275eda70a98ab63deccc1e380c6fdd246879366
john19981016 <john19981016@gmail.com>
2023-12-05 19:01:04 +0800

    Initial commit

---
+++ env_var.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/yanyangji/.mujoco/mujoco210/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+export CPATH=$CONDA_PREFIX:/home/yanyangji/anaconda3/envs/decdiff/include
+export PYTHONPATH=/home/yanyangji/research/RL/final/decision-diffuser/code
